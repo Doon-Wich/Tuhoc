1c07a7ccf0fdfe209b47eae4ed9ebab8019aaf88
TriNguyen <120323857+Doon-Wich@users.noreply.github.com>
2025-09-16 07:59:50 +0700

    khoitao

---
+++ mypro/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from './util/axios.customize'
+
+function App() {
+  
+  useEffect(() => {
+    const fetchHelloWorld = async() => {
+      const res = await axios.get(`/v1/api`)
+      console.log(res);
+    }
+
+    fetchHelloWorld();
+  }, [])
+
+  return (
+    <>
+      Hello world
+    </>
+  )
+}
+
+export default App
